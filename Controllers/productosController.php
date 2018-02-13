f3b98cd207723debffd1f82898a8ab062f3abe3e
Diego Pennisi <diegopennisi@outlook.es>
2018-02-13 15:14:57 -0300

    inventario terminado

--- Controllers/productosController.php
+++ Controllers/productosController.php
@@ -19,6 +19,18 @@
 
         }
 
+             public function getProductosBajaController(){
+      
+          $respuesta = ProductosModel::getProductosBajaModel('productos');
+
+           if ($respuesta) {
+            
+              echo json_encode($respuesta);
+           
+           }
+
+        }
+
 
  
         public function comprobarProductoController(){
@@ -41,8 +53,8 @@
 
    	  public function addProductosController(){
    	      $data = file_get_contents("php://input");
-		  $request = json_decode($data);
-		  $request = (array) $request;
+		      $request = json_decode($data);
+		      $request = (array) $request;
         
           $nombreProducto =$request['nombreProducto'];
           $idProveedor =$request['idProveedor'];
@@ -66,6 +78,115 @@
  			 }
 
    	    }
+
+         public function deleteProductosController(){
+          $data = file_get_contents("php://input");
+          $request = json_decode($data);
+          $request = (array) $request;
+
+          $idProducto =$request['idProducto'];
+      
+           $respuesta = ProductosModel::deleteProductosModel($idProducto, 'productos' );
+
+               if ($respuesta == 'success') {
+                
+                  echo json_encode($respuesta);
+               
+               }else{
+                echo json_encode($respuesta);
+               }
+        }
+
+        public function bajaProductosController(){
+          $data = file_get_contents("php://input");
+          $request = json_decode($data);
+          $request =(array) $request;
+
+          $idProducto = $request['idProducto'];
+
+            $respuesta = ProductosModel::bajaProductosModel($idProducto , 'productos');
+            if ($respuesta == 'success') {
+                
+                  echo json_encode($respuesta);
+               
+               }else{
+                echo json_encode($respuesta);
+               }
+
+        }
+
+          public function altaProductosController(){
+          $data = file_get_contents("php://input");
+          $request = json_decode($data);
+          $request =(array) $request;
+
+          $idProducto = $request['idProducto'];
+
+            $respuesta = ProductosModel::altaProductosModel($idProducto , 'productos');
+            if ($respuesta == 'success') {
+                
+                  echo json_encode($respuesta);
+               
+               }else{
+                echo json_encode($respuesta);
+               }
+
+        }
+
+
+    //
+    // INVENTARIO
+    //
+    // public function validarProductoController($validarProducto)
+    // {
+    //     $datosController = $validarProducto;
+    //     $respuesta = ProductosModel::validarProductoModel($datosController, 'productos');
+    //     if ($respuesta) {
+    //         echo 1;
+    //     } else {
+    //         echo 0;
+    //     }
+    // }
+
+    public  function getInventarioController()
+    {
+        $respuesta = ProductosModel::getInventarioModel('inventario');
+
+         echo json_encode($respuesta);
+    }
+
+    public function agregarInventarioController()
+    {
+
+          $data = file_get_contents("php://input");
+          $request = json_decode($data);
+          $request = (array) $request;
+          
+          $cantidadIngresada =$request['cantidadIngresada'];
+          $precioVenta =$request['precioVenta'];
+          $idProducto =$request['idProducto'];
+          
+    
+            $respuesta = ProductosModel::agregarInventarioModel(
+              $cantidadIngresada,
+              $precioVenta,
+              $idProducto, 
+              'inventario');
+           
+                 
+                  if ($respuesta == 'success') {
+                    
+                      echo json_encode($respuesta);
+                   
+                   }else{
+                    echo json_encode("no hay ingreso");
+                   }
+           
+     
+    }
+
+
+
    }
 
   
@@ -80,6 +201,13 @@
       }
    }
 
+     if(isset($_GET['id'])){
+      if ($_GET['id'] == "getProdBaja") {
+         $cat=new ProductosControllers;
+         $cat->getProductosBajaController();
+      }
+   }
+
 
      if(isset($_GET['id'])){
       if ($_GET['id'] == "comprobar") {
@@ -91,5 +219,40 @@
    	  if ($_GET['id'] == "addProd") {
          $cat=new ProductosControllers;
          $cat->addProductosController();
+      }
+   }
+
+        if(isset($_GET['id'])){
+      if ($_GET['id'] == "delete") {
+        $delete = new ProductosControllers;
+        $delete->deleteProductosController();  
+      }
+   }
+
+     if(isset($_GET['id'])){
+      if ($_GET['id'] == "baja") {
+        $delete = new ProductosControllers;
+        $delete->bajaProductosController();  
+      }
+   }
+    if(isset($_GET['id'])){
+      if ($_GET['id'] == "alta") {
+        $delete = new ProductosControllers;
+        $delete->altaProductosController();  
+      }
+   }
+
+
+   if(isset($_GET['id'])){
+      if ($_GET['id'] == "getInven") {
+         $inventario=new ProductosControllers;
+         $inventario->getInventarioController();
+      }
+   }
+
+      if(isset($_GET['id'])){
+      if ($_GET['id'] == "addInven") {
+         $inventario=new ProductosControllers;
+         $inventario->agregarInventarioController();
       }
    }
--- Models/productosModel.php
+++ Models/productosModel.php
@@ -6,7 +6,7 @@
    	   public function getProductosModel($table){
 
 
-   	   	    $sql = Conexion::conectar()->prepare("SELECT * FROM $table");
+   	   	    $sql = Conexion::conectar()->prepare("SELECT * FROM $table ta  JOIN categorias cat ON cat.idCategoria = ta.idCategoria WHERE estado = 1");
 
    	   	    if ($sql->execute()) {
    	   	    	 return $sql->fetchAll();
@@ -16,6 +16,20 @@
    	   	    }
             $sql->close();
    	   }
+
+         public function getProductosBajaModel($table){
+
+
+            $sql = Conexion::conectar()->prepare("SELECT * FROM $table ta  JOIN categorias cat ON cat.idCategoria = ta.idCategoria WHERE estado = 0");
+
+            if ($sql->execute()) {
+               return $sql->fetchAll();
+
+            }else{
+              return 'error';
+            }
+            $sql->close();
+       }
 
         public function comprobarProductosModel($datosModel , $table){
 
@@ -46,6 +60,15 @@
               $sql->bindParam(':precioProducto', $precioProducto);
               $sql->bindParam(':idCategoria', $idCategoria);
                 if($sql->execute()){
+                  // aqui agrega al inventario con el correspondiente idProducto para su relacion
+            $ult = Conexion::conectar()->prepare("SELECT MAX(idProducto)as ID FROM productos");
+            $ult->execute();
+            $res = $ult->fetch();
+            // var_dump($res['ID']);
+            $a = $res['ID'];
+            $sqlInv = Conexion::conectar()->prepare("INSERT INTO inventario(cantidadIngresada, precioVenta,idProducto)
+                        VALUES(0,0,$a)");
+            $sqlInv->execute();
 
                   return 'success';
                 }else{
@@ -55,5 +78,90 @@
                   $sql->close();
          }
 
+           public static function deleteProductosModel($datosModel, $tabla)
+    {
+
+        $sql = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE idProducto = :idProducto");
+        $sql->bindParam(':idProducto', $datosModel);
+
+        if ($sql->execute()) {
+            return 'success';
+        }
+        $sql->close();
+    }
+
+     public function bajaProductosModel($idProducto , $tabla){
+
+
+                $sql = Conexion::conectar()->prepare("UPDATE $tabla SET  estado = 0 WHERE idProducto = :idProducto");
+                
+                if($sql->execute(array(
+                   ':idProducto'=>$idProducto
+                 ))){
+
+                  return 'success';
+                }else{
+                  return 'Error';
+                }
+
 
       }
+
+         public function altaProductosModel($idProducto , $tabla){
+
+
+                $sql = Conexion::conectar()->prepare("UPDATE $tabla SET  estado = 1 WHERE idProducto = :idProducto");
+                
+                if($sql->execute(array(
+                   ':idProducto'=>$idProducto
+                 ))){
+
+                  return 'success';
+                }else{
+                  return 'Error';
+                }
+
+
+      }
+
+      //////
+    //  INVENTARIO.
+    //////
+
+    public  function getInventarioModel($tabla)
+    {
+        $sql = Conexion::conectar()->prepare("SELECT * FROM $tabla ta 
+          JOIN productos pro ON pro.idProducto = ta.idProducto  ");
+        if ($sql->execute()) {
+               return $sql->fetchAll();
+
+            }else{
+              return 'error';
+            }
+            $sql->close();
+    }
+
+    public  function agregarInventarioModel(
+      $cantidadIngresada,
+      $precioVenta,
+      $idProducto,
+      $tabla)
+    {
+
+        $sql = Conexion::conectar()->prepare(" UPDATE $tabla SET 
+          cantidadIngresada= cantidadIngresada + :cantidadIngresada,precioVenta=:precioVenta WHERE idProducto =:idProducto");
+
+        $sql->bindParam(':cantidadIngresada', $cantidadIngresada);
+        $sql->bindParam(':precioVenta', $precioVenta);
+        $sql->bindParam(':idProducto', $idProducto);
+        if ($sql->execute()) {
+            return 'success';
+        } else {
+            return 'Error';
+        }
+
+        $sql->close();
+    }
+
+
+    }
